66593901327525a9a5a21b004d80092d609bce29
Mimi Lin <hloople@gmail.com>
2015-04-11 15:05:01 -0400

    new format

--- fmt/fmt.cpp
+++ fmt/fmt.cpp
@@ -4,20 +4,11 @@
 #include <streambuf>
 #include <string>
 #include <dlfcn.h>
-//#include "fmt.h"
 #include "../unicode/utf.h"
 #include "../unicode/utf.cpp"
 #include "../unicode/word_break.h"
 
 int main(int argc, char *argv[]) {
-
-
-    word_break wb("1.21 gigawatts."); // convert std::string to char* str.c_str()
-    std::cout << wb.next() << std::endl;
-    std::cout << wb.next() << std::endl;
-    std::cout << wb.next() << std::endl;
-    std::cout << wb.next() << std::endl;
-
 
     if (argc != 3) {
         std::cout << "usage: " << argv[0] << " filename max_width" << std::endl;
@@ -29,41 +20,17 @@ int main(int argc, char *argv[]) {
                     std::istreambuf_iterator<char>());
     utf::encoding_type encoding = utf::detect_encoding(str);
     unsigned long line_width = 0;
-    std::string current_space = "";
-    std::string current_word = "";
-    const size_t total_length = utf::get_length(str, encoding);
-    for (size_t i = 0, byte_i = 0; i <= total_length;
-         i++, byte_i += i == total_length ? 0 :
-         utf::get_char_size(str, byte_i, encoding))
-    {
-        const uint32_t code_point =
-            i == total_length ? 0 : utf::get_char(str, byte_i, encoding);
-        if (i == total_length || utf::is_whitespace(code_point)) {
-            const unsigned long current_space_length =
-                utf::get_length(current_space, encoding);
-            const unsigned long current_word_length =
-                utf::get_length(current_word, encoding);
-            if (line_width + current_space_length + current_word_length >
-                max_width)
-            {
-                std::cout << std::endl;
-                line_width = 0;
-            } else {
-                std::cout << current_space;
-                line_width += current_space_length;
-            }
-            std::cout << current_word;
-            line_width += current_word_length;
-            current_space = "";
-            current_word = "";
-            if (i != total_length && utf::is_newline(code_point)) {
-                std::cout << std::endl;
-                line_width = 0;
-            } else {
-                add_char(current_space, code_point, encoding);
-            }
-        } else {
-            add_char(current_word, code_point, encoding);
+    word_break wb(str.c_str());
+    char * next = wb.next();
+
+    while(next != NULL) {
+        std::string current_word(next);
+        line_width = line_width + utf::get_length(current_word, encoding);
+        if(line_width > max_width) {
+            std::cout << "\n";
+            line_width = 0;
         }
+        std::cout << current_word;
+        next = wb.next();
     }
 }

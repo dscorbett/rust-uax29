9c0fb60b1db0cbb232042b60c01d6ba27414ec6e
Mimi Lin <hloople@gmail.com>
2015-04-11 20:36:47 -0400

    added purpose statements

--- unicode/word_break.h
+++ unicode/word_break.h
@@ -1,3 +1,10 @@
+/* Class word_break
+ * 
+ * word_break implements the iterator which follows 
+ * the default UAX #29 word-breaking specification.
+ *
+ */
+
 #ifndef WORDBREAK_H
 #define WORDBREAK_H
 
@@ -8,8 +15,13 @@ typedef const char *(*next_word_t)(rust_handle *);
 class word_break {
 
     public:
+        // Construct the word_break iterator
         word_break(const char *str);
+        
+        // Destruct the word_break_iterator
         ~word_break();
+        
+        // Get the next work in the iterator
         const char *next();
 
     private: 
@@ -17,6 +29,8 @@ class word_break {
         rust_handle *r_handle;
         create_wb_t fn_create_word_breaker;
         next_word_t fn_next_word;
+
+        // Close and nullify the links to the dll file
         void close_dll();
 };
 #endif
